7dbe96c26aefa94c4a6b93448bafa8eada552467
Josh <jheverett1988@gmail.com>
2017-04-09 14:03:59 -0400

    stub of multiple machine scanner

--- RCat.rkt
+++ RCat.rkt
@@ -2,6 +2,25 @@
 (require racket/system)
 (require racket/tcp)
 
+; https://docs.racket-lang.org/guide/regexp-match.html
+; > (regexp-split #rx":" "/bin:/usr/bin:/usr/bin/X11:/usr/local/bin")
+; '("/bin" "/usr/bin" "/usr/bin/X11" "/usr/local/bin")
+; > (regexp-split #rx" " "pea soup")
+; '("pea" "soup")
+; > (regexp-split #rx"" "smithereens")
+; '("" "s" "m" "i" "t" "h" "e" "r" "e" "e" "n" "s" "")
+; > (regexp-split #rx" +" "split pea     soup")
+; '("split" "pea" "soup")
+; > (regexp-split #rx" *" "split pea     soup")
+; '("" "s" "p" "l" "i" "t" "" "p" "e" "a" "" "s" "o" "u" "p" "")
+
+;; break up address beginning to end
+;;   192.168.1.1-10 -> '("192.168.1.1" "10")
+;; we want to return a new list of strings for every address between these endpoints -> '("192.168.1.1" "192.168.1.2" ... "192.168.1.10")
+;; we can pick apart this list to create a new list of machine objects that we feed the individual IPs to
+
+(define (RCat targets ports protocols)
+  (if (regexp-match? #rx".*-.*" targets) "yes" "no"))
 
 (define (machine ips ports protocols)
   (define open-tcp '())
@@ -30,13 +49,13 @@
      (thunk (apply system* proc args)))))
 ; ((execute-command "ping") '("-c 3" "8.8.8.8"))
 ; (regexp-match? #rx"0x" "...some-string...")
-(if (regexp-match? #rx".*64.*" ((execute-command "ping") '("-c 3" "8.8.8.8"))) "Yes" "no")
-(if (regexp-match? #rx".*64.*" ((execute-command "ping") '("-c 3" "192.168.6.66"))) "Yes" "no")
+;(if (regexp-match? #rx".*64.*" ((execute-command "ping") '("-c 3" "8.8.8.8"))) "Yes" "no")
+;(if (regexp-match? #rx".*64.*" ((execute-command "ping") '("-c 3" "192.168.6.66"))) "Yes" "no")
 
 ; attempt a connection with a non existent port
-(with-handlers ([exn:fail? (lambda (exn) 'air-bag)])
-    (let-values (((input output) (tcp-connect "8.8.8.8" 5555)))
-                (list input output)))
+;(with-handlers ([exn:fail? (lambda (exn) 'air-bag)])
+;    (let-values (((input output) (tcp-connect "8.8.8.8" 5555)))
+;                (list input output)))
 
 
 ; OK, engine might not be the best method to do thread creation. After some testing, it like engine expects to evaluate at runtime and then await a signal for execution of body.
