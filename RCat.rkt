f9c07c1fda7e0d4cda3d781efc1ed0cf478a2429
Josh <jheverett1988@gmail.com>
2017-04-17 16:28:52 -0400

    ongoing issues with multiple machine object encapsulation

--- RCat.rkt
+++ RCat.rkt
@@ -1,44 +1,53 @@
 #lang racket
 (require racket/system)
 (require racket/tcp)
+(define machine-list '())
 
+(display "NCat usage\n")
+(display "Usage examples")
 (define (RCat targets ports protocols)
   (if (regexp-match? #rx".*-.*" targets)
       (ips->machines targets ports protocols)
       (machine targets ports protocols)))
 
+
 (define (ips->machines targets ports protocols)
-  (define target-machine-ips '())
-  (define machine-list '())
+  (let ((target-machine-ips '()))
   (define (add-machine-alive ip)
     (set! target-machine-ips (cons ip target-machine-ips)))
-  (define (insert-machine machine)
-    (set! machine-list (cons machine machine-list)))
-  (define (check-tports port)
-    (map (lambda (machine) (if (machine '(check-tport port)) (machine '(ip)) " " )) machine-list))
   (define (check-uports port) "stub")
-;> (define ping-input '())
-;> (define x (car (process (string-append "ping -c 3 " addr))))
-;> (if (eof-object? (read-line x)) "hit EOF" (set! ping-input (cons (read-line x) ping-input )))
-;> ping-input
-;'("64 bytes from 192.168.1.1: icmp_seq=0 ttl=64 time=13.079 ms")
   (define (probe-ping addr)
     (thread (lambda ()
               (let ((ping-input '()))
                 (if (regexp-match? #rx".*64.*" (read-string 4096 (car (process (string-append "ping -c 3 " addr)))))
-                    (add-machine-alive addr)
+                    (set! machine-list (cons (machine addr ports protocols) machine-list));add-machine-alive addr)
                     "No connection detected")))))
   (define (dispatch message)
+    ;(define machine-list '())
+    (set! machine-list (map (lambda (open-ip) (machine open-ip ports protocols)) target-machine-ips))
     (cond((eq? (car message) 'up) target-machine-ips)
-         ((eq? (car message) 'machines) "test")
+         ((eq? (car message) 'machines) machine-list )
          ((eq? (car message) 'tport) (check-tports (cdr message)) )
          ((eq? (car message) 'uport) (check-uports (cdr message)) )
          (else error "Bad moves, dude")))
   (begin
     (map (lambda (target-ip) (probe-ping target-ip) ) (range->list targets))
-    (map (lambda (open-ip) (insert-machine (machine open-ip ports protocols)) ) target-machine-ips)
-  dispatch))
-  
+  dispatch)))
+
+;> (define z (ips->machines "8.8.8.8-9" "53" "t"))
+;> (z '(machines))
+;'(#<procedure:dispatch>)
+;> (define test (z '(machines)))
+;> (car test)
+;#<procedure:dispatch>
+;> ((car test) '(ip))
+;"8.8.8.8"
+;> ((car test) '(tport 53))
+;#t
+;> 
+
+(define (check-tports port)
+  (map (lambda (machine-dispatch) (if(machine-dispatch (list 'tport port)) (machine-dispatch '(ip)) " ")) machine-list))
 
 ; convert from range of ips to a list of ips
 ; (range->list "192.168.1-15") -> '("192.168.1.1" ... "192.168.1.15")
@@ -71,11 +80,14 @@
   (define (check-uport port)
     (if (memq port open-udp) #t #f))
   (define (check-tport port)
-    (if (memq port open-tcp) #t #f))
+    (if (memq (string->number port) open-tcp) #t #f))
   (define (probe-tcp ip port)
-    (thread (lambda () (with-handlers ([exn:fail? (lambda (exn) exn )])(if 
-    (let-values (((input output) (tcp-connect ip port)))
-                (list input output)) (add-tcp port) "NO")))))
+    (display "MAKING THREAD")
+    (thread (lambda () (with-handlers ([exn:fail? (lambda (exn) exn )])
+                         (if
+                          (let-values (((input output) (tcp-connect ip port))) (list input output))
+                          (add-tcp port)
+                          "No connection detected")))))
   (define (dispatch message)
     (cond((eq? (car message) 'tports) open-tcp)
          ((eq? (car message) 'uports) open-udp)
@@ -86,10 +98,22 @@
   (begin (map (lambda (x) (probe-tcp ip x)) (enum-ports ports)) dispatch))
 
 (define (enum-ports ports)
-  (if (regexp-match? #rx".*-.*" ports)
+  (if
+   (regexp-match? #rx".*-.*" ports)
   (let*((range(regexp-split #rx"-" ports))
         (start (car range))
         (end (cadr range))
         (port-range-numbers(enum-range-i (string->number start) (string->number end)))
         (port-range-strings (map number->string port-range-numbers)))
     port-range-numbers) (list (string->number ports)) ))
+
+
+
+;;; notes
+
+; ping concept + notes on reading port streams
+;> (define ping-input '())
+;> (define x (car (process (string-append "ping -c 3 " addr))))
+;> (if (eof-object? (read-line x)) "hit EOF" (set! ping-input (cons (read-line x) ping-input )))
+;> ping-input
+;'("64 bytes from 192.168.1.1: icmp_seq=0 ttl=64 time=13.079 ms")
